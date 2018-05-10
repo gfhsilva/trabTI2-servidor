c47ccf1e345d32e99ee05a492242ac7865d7f3db
Guilherme Silva <Guilherme Silva@BALDAS>
2018-05-10 14:47:25 +0100

    versão inicial do projeto.

---
+++ trabalhoTi2Final/trabalhoTi2Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trabalhoTi2Final.Startup))]
+namespace trabalhoTi2Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
