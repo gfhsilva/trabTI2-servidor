55f18dbee6809322c83a62022eaf7e16d10c9e8d
Guilherme Silva <Guilherme Silva@BALDAS>
2018-06-08 17:04:42 +0100

    editar apagar e criar pratos feitos

--- trabalhoTi2Final/trabalhoTi2Final/Controllers/PratosController.cs
+++ trabalhoTi2Final/trabalhoTi2Final/Controllers/PratosController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using trabalhoTi2Final.Models;
@@ -48,16 +50,57 @@ namespace trabalhoTi2Final.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IDPratos,Image,Descricao,TipoPratoFK")] Pratos pratos)
+        public async Task<ActionResult> Create([Bind(Include = "IDPratos,Image,Descricao,TipoPratoFK")] Pratos pratos, HttpPostedFileBase fileUploadFotografia)
         {
+            
+            // determinar o ID do novo Agente
+            int novoID = 0;
+            // *****************************************
+            // proteger a geração de um novo ID
+            // *****************************************
+            // determinar o nº de tipo de pratos na tabela
+            if (db.Pratos.Count() == 0)
+            {
+                novoID = 1;
+            }
+            else
+            {
+                novoID = db.Pratos.Max(a => a.IDPratos) + 1;
+            }
+            // atribuir o ID ao novo agente
+            pratos.IDPratos = novoID;
+            // ***************************************************
+            // outra hipótese possível seria utilizar o
+            // try { }
+            // catch(Exception) { }
+            // ***************************************************
+
+            // var. auxiliar
+            string nomeFotografia = "pratos" + novoID + ".jpg";
+            string caminhoParaFotografia = Path.Combine(Server.MapPath("~/images/"), nomeFotografia); // indica onde a imagem será guardada
+
+            // verificar se chega efetivamente um ficheiro ao servidor
+            if (fileUploadFotografia != null)
+            {
+                // guardar o nome da imagem na BD
+                pratos.Image = nomeFotografia;
+            }
+            else
+            {
+                // não há imagem...
+                ModelState.AddModelError("", "Não foi fornecida uma imagem..."); // gera MSG de erro
+                return View(pratos); // reenvia os dados do 'Agente' para a View
+            }
+
             if (ModelState.IsValid)
             {
+                // guardar a imagem no disco rígido
+                fileUploadFotografia.SaveAs(caminhoParaFotografia);
                 db.Pratos.Add(pratos);
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.TipoPratoFK = new SelectList(db.TipoPratos, "IDTipoPrato", "Designacao", pratos.TipoPratoFK);
             return View(pratos);
         }
 
@@ -82,15 +125,52 @@ namespace trabalhoTi2Final.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IDPratos,Image,Descricao,TipoPratoFK")] Pratos pratos)
+        public async Task<ActionResult> Edit([Bind(Include = "IDPratos,Image,Descricao,TipoPratoFK")] Pratos pratos, HttpPostedFileBase uploadFoto)
         {
+            // vars. auxiliares
+            string novoNome = "";
+            string nomeAntigo = "";
+
             if (ModelState.IsValid)
             {
-                db.Entry(pratos).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                try
+                {              /// se foi fornecida uma nova imagem,
+                               /// preparam-se os dados para efetuar a alteração
+                    if (uploadFoto != null)
+                    {
+                        /// antes de se fazer alguma coisa, preserva-se o nome antigo da imagem,
+                        /// para depois a remover do disco rígido do servidor
+                        nomeAntigo = pratos.Image;
+                        /// para o novo nome do ficheiro, vamos adicionar o termo gerado pelo timestamp
+                        /// devidamente formatado, mais
+                        /// A extensão do ficheiro é obtida automaticamente em vez de ser escrita de forma explícita
+                        novoNome = "tipoPrato" + pratos.IDPratos + DateTime.Now.ToString("_yyyyMMdd_hhmmss") + Path.GetExtension(uploadFoto.FileName).ToLower(); ;
+                        /// atualizar os dados do Agente com o novo nome
+                        pratos.Image = novoNome;
+                        /// guardar a nova imagem no disco rígido
+                        uploadFoto.SaveAs(Path.Combine(Server.MapPath("~/images/"), novoNome));
+                    }
+
+                    // guardar os dados do Agente
+                    db.Entry(pratos).State = EntityState.Modified;
+                    // Commit
+                    db.SaveChanges();
+
+                    /// caso tenha sido fornecida uma nova imagem há necessidade de remover 
+                    /// a antiga
+                    if (uploadFoto != null)
+                        System.IO.File.Delete(Path.Combine(Server.MapPath("~/images/"), nomeAntigo));
+
+
+                    // enviar os dados para a página inicial
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    // caso haja um erro deve ser enviada uma mensagem para o utilizador
+                    ModelState.AddModelError("", string.Format("Ocorreu um erro com a edição dos dados do tipo de Prato {0}", pratos.Descricao));
+                }
             }
-            ViewBag.TipoPratoFK = new SelectList(db.TipoPratos, "IDTipoPrato", "Designacao", pratos.TipoPratoFK);
             return View(pratos);
         }
 
--- trabalhoTi2Final/trabalhoTi2Final/Models/Pratos.cs
+++ trabalhoTi2Final/trabalhoTi2Final/Models/Pratos.cs
@@ -14,6 +14,7 @@ namespace trabalhoTi2Final.Models
         public int IDPratos { get; set; }
 
         public string Image { get; set; }
+        
         public string Descricao { get; set; }
      
   
